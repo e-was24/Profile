870d841b8f6fa24882f42a33e0e7d275fca75267
Haii <168735717+e-was24@users.noreply.github.com>
2024-08-20 23:06:48 +0700

    Update style.js

--- style.js
+++ style.js
@@ -1,2 +1,14 @@
-const nav  = document.querySelector(".nav").cloneNode(true);
-document.querySelector(".container").appendChild(nav)
+function waktu() {
+    var now = new Date();
+    var day = now.getDate();
+    var month = now.getMonth() + 1;
+    var year = now.getFullYear();
+
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+
+    var timeString = day + ' ' + '|' + ' ' + month + ' ' + '|' + ' ' + year;
+    document.getElementById('waktu').innerText = timeString
+}
+
+setInterval(waktu, 1000)
